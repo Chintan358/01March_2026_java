77c06b4dab6c5b10aa9b43e73e61acd72e5835c8
chintan <chintan@gmail.com>
2026-04-04 12:17:53 +0530

    oops

--- Learning_corejava/src/oops/O001_ClassDemo.java
+++ Learning_corejava/src/oops/O001_ClassDemo.java
@@ -19,7 +19,7 @@ class Pen
 
 public class O001_ClassDemo 
 {
-	public  void main(String[] args)
+	public static void main(String[] args)
 	{
 		Pen.company = "SS";
 		
---
+++ Learning_corejava/src/oops/O003_ObjectCount.java
@@ -0,0 +1,27 @@
+package oops;
+
+
+class Demo
+{
+	static int count = 0;
+	Demo()
+	{
+		count++;
+		//System.out.println("const calling"+count);
+		
+	}
+}
+public class O003_ObjectCount {
+	public static void main(String[] args) {
+		
+		Demo d1 = new Demo();
+		Demo d2 = new Demo();
+		Demo d3 = new Demo();
+		Demo d4 = new Demo();
+		Demo d5 = new Demo();
+		
+		
+		System.out.println("Total : "+Demo.count);
+		
+	}
+}
